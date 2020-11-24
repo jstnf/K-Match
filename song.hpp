ccaa9a3030d12da266bc7650b972e79d694c2d10
nateknguyen <github.delesseria@aleeas.com>
2020-11-23 21:05:07 -0800

    Initialize song class files

---
+++ song.hpp
@@ -0,0 +1,19 @@
+#ifndef __SONG_HPP__
+#define __SONG_HPP__
+
+#include <string>
+#include <iostream>
+
+
+class Song 
+{
+    public:
+        Song() {};
+        string getSongName(void);
+        string SongName;
+        string SongArtist;
+        string Song
+
+    private:
+        void setSongName();
+}
