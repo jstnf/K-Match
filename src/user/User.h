a96040e287d23db8bba2e2ca648b43a5950aa4db
Nathan Nguyen <github.delesseria@aleeas.com>
2020-12-08 12:34:56 -0800

    Update User.h

--- src/user/User.h
+++ src/user/User.h
@@ -1,17 +1,30 @@
 #ifndef __USER_H__
 #define __USER_H__
 
+#include "UserTrackList.h"
+
 #include <string>
 
 class User {
   private:
-    std::string id;
+    const std::string id; // alphanumeric and underscore ONLY
+    UserTrackList* utl;
 
   public:
-    User(std::string id) : id(id) { }
+    User(std::string id, std::string trackListName) : id(id) {
+        utl = new UserTrackList(trackListName);
+    }
+
+    ~User() {
+        delete utl;
+    }
 
     std::string getId() const {
         return id;
+    }
+    
+    UserTrackList* getUTL() const {
+        return utl;
     }
 };
 
