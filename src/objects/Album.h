79907358595a8568296a92dba44f0c3a7eba6dcf
Justin <jstnf@users.noreply.github.com>
2020-12-07 22:32:57 -0800

    Update Album and Track to use Spotify specs

--- src/objects/Album.h
+++ src/objects/Album.h
@@ -1,27 +1,17 @@
 #ifndef __ALBUM_H__
 #define __ALBUM_H__
 
+#include <ctime>
 #include <string>
-#include <vector>
 
 class Album {
   private:
     std::string id;
     std::string name;
-    std::vector<std::string>* artistIds;
+    std::time_t releaseTime;
 
   public:
-    Album(std::string id, std::string name) : id(id), name(name) {
-        artistIds = new std::vector<std::string>();
-    }
-
-    ~Album() {
-        delete artistIds;
-    }
-
-    void addArtist(std::string artistId) {
-        artistIds->push_back(artistId);
-    }
+    Album(std::string id, std::string name, std::time_t releaseTime) : id(id), name(name), releaseTime(releaseTime) { }
 
     std::string getId() {
         return id;
@@ -31,8 +21,8 @@ class Album {
         return name;
     }
 
-    std::vector<std::string>* getArtistIds() {
-        return artistIds;
+    std::time_t getReleaseTime() {
+        return releaseTime;
     }
 };
 
--- src/objects/Track.h
+++ src/objects/Track.h
@@ -2,29 +2,17 @@
 #define __TRACK_H__
 
 #include <string>
-#include <vector>
 
 class Track {
   private:
     std::string id;
     std::string name;
-    std::vector<std::string>* artistIds;
     std::string albumId;
     int durationMs;
     int popularity;
     
   public:
-    Track(std::string name, std::string albumId, int durationMs, int popularity) : name(name), albumId(albumId), durationMs(durationMs), popularity(popularity) {
-        artistIds = new std::vector<std::string>();
-    }
-
-    ~Track() {
-        delete artistIds;
-    }
-
-    void addArtist(std::string artistId) {
-        artistIds->push_back(artistId);
-    }
+    Track(std::string name, std::string albumId, int durationMs, int popularity) : name(name), albumId(albumId), durationMs(durationMs), popularity(popularity) { }
 
     std::string getId() {
         return id;
@@ -32,10 +20,6 @@ class Track {
 
     std::string getName() {
         return name;
-    }
-
-    std::vector<std::string>* getArtistIds() {
-        return artistIds;
     }
 
     std::string getAlbumId() {
