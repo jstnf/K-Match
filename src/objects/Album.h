20ea179959a19f0b3ab403f60707b096b8c65dc9
Justin <jstnf@users.noreply.github.com>
2020-12-08 04:43:11 -0800

    Use a string for release date instead of time_t

--- src/objects/Album.h
+++ src/objects/Album.h
@@ -3,17 +3,15 @@
 
 #include "KMObject.h"
 
-#include <ctime>
-
 class Album : public KMObject {
   private:
-    const std::time_t releaseTime;
+    const std::string releaseDate;
 
   public:
-    Album(std::string id, std::string name, std::time_t releaseTime) : KMObject(id, name), releaseTime(releaseTime) { }
+    Album(std::string id, std::string name, std::string releaseDate) : KMObject(id, name), releaseDate(releaseDate) { }
 
-    std::time_t getReleaseTime() const {
-        return releaseTime;
+    std::string getReleaseDate() const {
+        return releaseDate;
     }
 };
 
