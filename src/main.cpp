6983aeb12458c36c5dcff916e813823ada3c54e4
Justin <jstnf@users.noreply.github.com>
2020-12-08 08:01:00 -0800

    Update main.cpp with search engine example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "io/ObjectLoader.h"
+#include "objects/SearchEngine.h"
 
 #include <iostream>
-// #include <Windows.h>
+#include <Windows.h>
+
+void search(ObjectManager* om, SearchEngine* se, std::string keyword);
 
 int main() {
     ObjectManager* om = new ObjectManager();
@@ -14,20 +17,32 @@ int main() {
 
     std::cout << "Hello world!" << std::endl;
 
+    SearchEngine* se = new SearchEngine(om);
+
     // Test code below
-
-    /*
-    std::cout << "There are " << std::endl
-              << om->getAlbums()->size() << " albums, " << std::endl
-              << om->getArtists()->size() << " artists, and" << std::endl
-              << om->getTracks()->size() << " tracks loaded." << std::endl;
-
     SetConsoleOutputCP(CP_UTF8);
 
-    auto it = om->getTracks()->begin();
-    while (it != om->getTracks()->end()) {
-        Track* t = it->second;
-        std::cout << t->getName() << " - ";
+    std::string str;
+    int searches = 0;
+    while (searches < 3) {
+        getline(std::cin, str);
+        search(om, se, str);
+        searches++;
+    }
+
+    delete se;
+    delete om;
+    std::cout << "Goodbye! :)" << std::endl;
+    return 0;
+}
+
+void search(ObjectManager* om, SearchEngine* se, std::string keyword) {
+    TrackSort* sorter = new PopularitySort(); // Change the sort here
+
+    std::vector<Track*> results = se->search(keyword, *sorter);
+    for (auto it = results.begin(); it != results.end(); it++) {
+        Track* t = *it;
+        std::cout << "[" << t->getPopularity() << "] " << t->getName() << " - ";
         auto artistVect = om->getTrackArtists(t->getId());
         auto aIt = artistVect.begin();
         while (aIt != artistVect.end()) {
@@ -39,11 +54,5 @@ int main() {
                 std::cout << std::endl;
             }
         }
-        it++;
     }
-     */
-
-    delete om;
-    std::cout << "Goodbye! :)" << std::endl;
-    return 0;
 }
