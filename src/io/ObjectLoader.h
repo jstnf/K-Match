528d1679c93af506070df514f5adba9c9ac7b612
Justin <jstnf@users.noreply.github.com>
2020-12-08 04:56:12 -0800

    TSV reading implement attempt

--- src/io/ObjectLoader.h
+++ src/io/ObjectLoader.h
@@ -1,17 +1,20 @@
 #ifndef __OBJECTLOADER_H__
 #define __OBJECTLOADER_H__
 
+#include "../ObjectManager.h"
+
 class ObjectLoader {
   private:
-    const ObjectManager* om;
-
-  public:
-    ObjectLoader(ObjectManager* om) : om(om) { }
+    ObjectManager* om;
     bool readAlbums();
     bool readArtists();
     bool readTracks();
     bool readAlbumArtists();
     bool readTrackArtists();
+
+  public:
+    ObjectLoader(ObjectManager* om) : om(om) { }
+    bool loadObjects();
 };
 
 #endif
